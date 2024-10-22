32d951dc8664e5c5a5015ee1ad0f7696009768a3
Jhanel Lalor <lalorjhanel4@gmail.com>
2024-10-22 10:02:33 -0500

    Board Layout Setup

---
+++ tic-tac-toe.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('#board div');
+    squares.forEach(square => square.classList.add('square'));
+  });
+  
