a0c483f4d3cd195224617a025c611afd535cf0a7
Jhanel Lalor <lalorjhanel4@gmail.com>
2024-10-22 10:08:38 -0500

    Game restarts

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -47,4 +47,14 @@ document.querySelectorAll('.square').forEach(square => {
   document.querySelectorAll('.square').forEach(square => {
     square.addEventListener('click', checkWinner);
   });
+
+  document.querySelector('.btn').addEventListener('click', () => {
+    document.querySelectorAll('.square').forEach(square => {
+      square.textContent = '';
+      square.classList.remove('X', 'O');
+    });
+    document.getElementById('status').textContent = 'Move your mouse over a square and click to play an X or an O.';
+    document.getElementById('status').classList.remove('you-won');
+    currentPlayer = 'X';
+  });
   
